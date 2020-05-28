c9f72d7a25fa29945ca6b37e9ea17148fabba114
Jan Kristanto Wibisono <jankristanto.03g@g2.nctu.edu.tw>
2020-05-28 15:40:27 +0800

    update relu

--- model.py
+++ model.py
@@ -8,29 +8,24 @@ class Enrichment(nn.Module):
     def __init__(self, c_in, rate=2):
         super(Enrichment, self).__init__()
         self.rate = rate
+		self.relu = nn.ReLU(inplace=True)
         self.conv = nn.Conv2d(c_in, 32, 3, stride=1, padding=1)
-        self.relu = nn.ReLU(inplace=True)
         dilation = self.rate*1 if self.rate >= 1 else 1
         self.conv1 = nn.Conv2d(32, 32, 3, stride=1, dilation=dilation, padding=dilation)
-        self.relu1 = nn.ReLU(inplace=True)
         dilation = self.rate*2 if self.rate >= 1 else 1
         self.conv2 = nn.Conv2d(32, 32, 3, stride=1, dilation=dilation, padding=dilation)
-        self.relu2 = nn.ReLU(inplace=True)
         dilation = self.rate*3 if self.rate >= 1 else 1
         self.conv3 = nn.Conv2d(32, 32, 3, stride=1, dilation=dilation, padding=dilation)
-        self.relu3 = nn.ReLU(inplace=True)
         dilation = self.rate * 4 if self.rate >= 1 else 1
         self.conv4 = nn.Conv2d(32, 32, 3, stride=1, dilation=dilation, padding=dilation)
-        self.relu4 = nn.ReLU(inplace=True)
-
         self._initialize_weights()
 
     def forward(self, x):
         o = self.relu(self.conv(x))
-        o1 = self.relu1(self.conv1(o))
-        o2 = self.relu2(self.conv2(o))
-        o3 = self.relu3(self.conv3(o))
-        o4 = self.relu3(self.conv4(o))
+        o1 = self.relu(self.conv1(o))
+        o2 = self.relu(self.conv2(o))
+        o3 = self.relu(self.conv3(o))
+        o4 = self.relu(self.conv4(o))
         out = o + o1 + o2 + o3 + o4
         return out
 
